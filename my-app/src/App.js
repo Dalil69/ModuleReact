12cc37c6d842a69c15846ed14d662e91b087cab3
John Doe <johndoe@example.com>
2023-01-26 11:25:29 +0100

    okeok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 <link rel="stylesheet" href="index.css"></link>
 const prices = {
   'Voiture location': 900,
@@ -9,22 +10,45 @@ const prices = {
 };
 
 function App(props) {
-  const listItems = produit.map((produit, index) =>
+  const [sortOrder, setSortOrder] = useState('asc'); //état initial est croissant
+  const [produit, setProduit] = useState(['Voiture location', 'Abonnement Netflux', 'Pc portable', 'Micro']);
+
+  const sortedProduit = [...produit].sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return prices[a] - prices[b];
+    } else {
+      return prices[b] - prices[a];
+    }
+  });
+
+  const listItems = sortedProduit.map((produit, index) =>
     <li key={index}>{produit}<button onClick={() => alert(`Payment for ${produit}`)}>Pay {prices[produit]} euros</button></li>
   );
-  
+
   return (
     <div>
       <h1>C'est parti </h1>
+      <div>
+        <label>Filtrer par prix : </label>
+        <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+          <option value="asc">Croissant</option>
+          <option value="desc">Décroissant</option>
+        </select>
+      </div>
+        <div class="wave-container">
+        <div class="wave"></div>
+      </div>
+
       <ul>{listItems}</ul>
     </div>
   );
 }
-const produit = ['Voiture location', 'Abonnement Netflux', 'Pc portable', 'Micro'];
+
 ReactDOM.render(
-  <App produit={produit} />,
+  <App />,
   document.getElementById('root')
 );
 
 export default App;
+
 
